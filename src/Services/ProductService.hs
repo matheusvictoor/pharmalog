fafcc2ba0a8df076d7076ec03b5a54b7d9dfbbf9
Matheus Victor <matheusvictor.dev@gmail.com>
2024-08-31 15:06:02 -0300

    fix: conserta a funcao de buscar produto pelo nome

--- src/Services/ProductService.hs
+++ src/Services/ProductService.hs
@@ -1,23 +1,12 @@
 {-# LANGUAGE BangPatterns #-}
-module Services.ProductService 
-  ( createProduct
-  , getProductById
-  , deleteProduct
-  , updateProduct
-  , getAllProducts
-  , alertLowStockProducts
-  , alertExpiringProducts
-  ) where
+module Services.ProductService ( createProduct, deleteProduct, updateProduct, getAllProducts, alertLowStockProducts, alertExpiringProducts, menuProduct) where
+-- module Services.ProductService ( createProduct, getProductById, deleteProduct, updateProduct, getAllProducts, alertLowStockProducts, alertExpiringProducts, menuProduct) where
 
 import Models.Product
 import Data.List (find)
 import Data.Time.Clock()
-import Data.Time
-    ( UTCTime,
-      addUTCTime,
-      getCurrentTime,
-      defaultTimeLocale,
-      parseTimeM )
+import Data.Time ( UTCTime, addUTCTime, getCurrentTime, defaultTimeLocale, parseTimeM )
+import System.IO (hFlush, stdout)
 
 data Index a = Index { index :: Int, productData :: a } deriving (Show, Read)
 
@@ -31,7 +20,7 @@ createProduct = do
     <*> (putStrLn "Categoria: " >> getLine)
     <*> (putStrLn "Data de Fabricação (YYYY-MM-DD): " >> getLine >>= parseDate)
     <*> (putStrLn "Data de Expiração (YYYY-MM-DD): " >> getLine >>= parseDate)
-    <*> (putStrLn "Preço: " >> readLn)
+    <*> (putStrLn "Preço (9.99): " >> readLn)
     <*> (putStrLn "Estoque: " >> readLn)
   
   -- Salvar o novo produto no arquivo _productDB.dat
@@ -45,11 +34,25 @@ parseDate str =
     Just date -> return date
     Nothing   -> fail "Formato de data inválido. Use o formato YYYY-MM-DD."
 
-getProductById :: String -> IO (Maybe Product)
-getProductById searchName = do
+-- getProductById :: IO ()
+-- getProductById = do
+--   putStrLn "ID do produto para buscar: "
+--   productId <- readLn
+--   contents <- readFile "_productDB.dat"
+--   let products = map (productData . (read :: String -> Index Product)) (lines contents)
+--   return $ find (\p -> productId p == searchName) products of
+--     Just p -> putStrLn $ "Informações do produto:\n" ++ show p
+--     Nothing -> putStrLn "Produto não encontrado."
+
+getProductByName :: IO ()
+getProductByName = do
+  putStrLn "Nome do produto para buscar: "
+  productNameSearched <- getLine
   contents <- readFile "_productDB.dat"
-  let products = map (productData . read) (lines contents) :: [Product]
-  return $ find (\p -> nameProduct p == searchName) products
+  let products = map (read :: String -> Product) (lines contents)
+  case find (\p -> nameProduct p == productNameSearched) products of
+    Just p -> putStrLn $ "Informações do produto:\n" ++ show p
+    Nothing -> putStrLn "Produto não encontrado."
 
 deleteProduct :: String -> IO ()
 deleteProduct searchName = do
@@ -96,8 +99,6 @@ alertLowStockProducts limit = do
         then putStrLn $ "Alerta! Produto: " ++ nameProduct prod ++ " está com estoque baixo. Estoque atual: " ++ show (stock prod)
         else putStrLn $ "Produto: " ++ nameProduct prod ++ " está com estoque suficiente. Estoque atual: " ++ show (stock prod)
 
-
-
 alertExpiringProducts :: Int -> IO ()
 alertExpiringProducts daysBefore = do
   products <- getAllProducts
@@ -110,3 +111,34 @@ checkProductExpiration currentTime daysBefore prod = do
   if expirationDate prod <= expiringDate
     then putStrLn $ "Alerta! O produto \"" ++ nameProduct prod ++ "\" está perto de vencer ou já venceu. Data de Expiração: " ++ show (expirationDate prod)
     else putStrLn $ "O produto \"" ++ nameProduct prod ++ "\" está ok. Data de Expiração: " ++ show (expirationDate prod)
+  
+menuProduct :: IO ()
+menuProduct = do
+  putStrLn "Selecione uma opção:"
+  putStrLn "1.  Cadastrar um novo produto"
+  putStrLn "2.  Buscar um produto por ID"
+  putStrLn "3.  Buscar um produto por name"
+  putStrLn "4.  Buscar todos os produtos"
+  putStrLn "5.  Atualizar um produto"
+  putStrLn "6.  Remove um produto"
+  putStrLn "7.  Alertar sobre Baixo Estoque"
+  putStrLn "8.  Alertar sobre Produtos Perto de Vencer"
+  putStrLn "0 <- Voltar"
+
+  putStr "Opção -> "
+  hFlush stdout
+
+  option <- getLine
+  
+  case option of
+    "1" -> createProduct
+    -- "2" -> getProductById
+    "3" -> getProductByName
+    -- "4" -> getAllProducts
+    -- "5" -> updateProduct
+    -- "6" -> deleteProduct
+    -- "7" -> alertLowStockProducts
+    -- "8" -> alertExpiringProducts
+    "0" -> putStrLn "<---"
+    _ -> putStrLn "Opção inválida. Tente novamente." >> menuProduct
+  putStrLn ""
