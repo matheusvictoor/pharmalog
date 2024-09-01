538ccb9bebad76c59632a4cc76fb73136c80f2b0
Matheus Victor <matheusvictor.dev@gmail.com>
2024-09-01 13:22:19 -0300

    feat: adiciona o menu no model de vendas

--- src/Services/SaleService.hs
+++ src/Services/SaleService.hs
@@ -1,41 +1,42 @@
 {-# LANGUAGE BangPatterns #-}
-module Services.SaleService (createSale, getAllSales, getSaleByClientId, updateSale, deleteSale) where
+module Services.SaleService (createSale, getAllSales, getSaleByClientId, updateSale, deleteSale, menuSale) where
 
 import Models.Sale
 import Data.List (find, deleteBy)
 import Data.Time.Clock (UTCTime)
 import Data.Time.Format (defaultTimeLocale, parseTimeM)
+import System.IO (hFlush, stdout)
 
 data Index a = Index { index :: Int, saleData :: a } deriving (Show, Read)
 
 createSale :: IO ()
 createSale = do
-  !saleId <- fmap (length . lines) (readFile "_SaleDB.dat")
+  !saleId <- fmap (length . lines) (readFile "_saleDB.dat")
   sale <- Sale
-    <$> (putStrLn "ID do Cliente: " >> readLn)
+    <$> (putStrLn "CPF do Cliente: " >> readLn)
     <*> (putStrLn "ID do Vendedor: " >> readLn)
     <*> (putStrLn "Data da Venda (YYYY-MM-DD): " >> getLine >>= parseDate)
-    <*> (putStrLn "Valor da Venda: " >> readLn)
+    <*> (putStrLn "Valor da Venda (9.99): " >> readLn)
     <*> return []
-  appendFile "_SaleDB.dat" (show (Index (1+saleId) sale) ++ "\n")
+  appendFile "_saleDB.dat" (show (Index (1+saleId) sale) ++ "\n")
   putStrLn "** Venda registrada com sucesso! **"
 
 getAllSales :: IO [Sale]
 getAllSales = do
-  contents <- readFile "_SaleDB.dat"
+  contents <- readFile "_saleDB.dat"
   return $ map (saleData . read) (lines contents)
 
-getSaleByClientId :: Int -> IO (Maybe Sale)
+getSaleByClientCpf :: Int -> IO (Maybe Sale)
 getSaleByClientId searchClientId = do
   sales <- getAllSales
   return $ find (\sale -> clientId sale == searchClientId) sales
 
 updateSale :: Int -> IO ()
 updateSale searchClientId = do
-  contents <- readFile "_SaleDB.dat"
+  contents <- readFile "_saleDB.dat"
   let sales = lines contents
   let updatedSales = map updateIfFound sales
-  writeFile "_SaleDB.dat" (unlines updatedSales)
+  writeFile "_saleDB.dat" (unlines updatedSales)
   putStrLn "** Venda atualizada com sucesso! **"
   where
     updateIfFound line =
@@ -52,10 +53,10 @@ updateSale searchClientId = do
 
 deleteSale :: Int -> IO ()
 deleteSale searchClientId = do
-  contents <- readFile "_SaleDB.dat"
+  contents <- readFile "_saleDB.dat"
   let sales = lines contents
   let filteredSales = filter (\line -> clientId (saleData (read line :: Index Sale)) /= searchClientId) sales
-  writeFile "_SaleDB.dat" (unlines filteredSales)
+  writeFile "_saleDB.dat" (unlines filteredSales)
   putStrLn "** Venda deletada com sucesso! **"
 
 parseDate :: String -> IO UTCTime
@@ -63,3 +64,24 @@ parseDate str =
   case parseTimeM True defaultTimeLocale "%Y-%m-%d" str of
     Just date -> return date
     Nothing   -> fail "Formato de data inválido. Use o formato YYYY-MM-DD."
+
+menuSale :: IO ()
+menuSale = do
+  putStrLn "\nSelecione uma opção:"
+  putStrLn "1.  Cadastrar um nova venda"
+  putStrLn "2.  Buscar um cliente por CPF"
+  putStrLn "3.  Buscar todos os clientes"
+  putStrLn "0 <- Voltar"
+
+  putStr "\nOpção -> "
+  hFlush stdout
+
+  option <- getLine
+
+  case option of
+    "1" -> createSale
+    "2" -> getSaleByClientCpf
+    -- "3" -> getAllSales
+    "0" -> putStrLn "\n<---"
+    _   -> putStrLn "Opção inválida. Tente novamente." >> menuSale
+  putStrLn ""
