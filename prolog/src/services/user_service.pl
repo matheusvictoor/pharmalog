e616c299351d253e58b67f58bea5316678811e83
Matheus Victor <matheusvictor.dev@gmail.com>
2024-10-06 09:33:02 -0300

    feat: adiciona a funcao para imprimir mensagens de aviso na tela

--- prolog/src/services/user_service.pl
+++ prolog/src/services/user_service.pl
@@ -11,14 +11,13 @@ menu_user :-
 handle_user_option(1) :-
   writeln("\nCPF: "), read(CPF),
   ( user_exists_cpf(CPF) -> 
-    writeln("\n** CPF já cadastrado! **"), menu_user
+    exibir_mensagem_formatada("CPF já cadastrado!"), menu_user
   ;
     writeln("Nome: "), read(Name),
     writeln("Senha: "), read(Password),
     writeln("Função (administrador | gerente | vendedor): "), read(Role),
     create_user(Name, CPF, Password, Role),
-    % write("\n*********** Usuário cadastrado com sucesso! **********"), nl, nl,
-    exibir_mensagem_formatada('Usuário cadastrado com sucesso!'), nl, nl,
+    exibir_mensagem_formatada('✓ Usuário cadastrado com sucesso!'), nl, nl,
     menu_user
   ).
 
@@ -26,10 +25,10 @@ handle_user_option(2) :-
   writeln("CPF do usuário para buscar: "), read(CPF),
   ( user_exists_cpf(CPF) ->
     get_user_by_cpf(CPF, User), nl,
-    writeln('*************** Informações do Usuário ***************'), nl,
+    exibir_mensagem_formatada('Informações do Usuário'), nl,
     print_users(User)
   ;
-    writeln("\n************** Usuário não encontrado! **************"), nl, nl
+    exibir_mensagem_formatada('✗ Usuário não encontrado!'), nl, nl
   ),
   menu_user.
   
@@ -43,9 +42,9 @@ handle_user_option(4) :-
     writeln("Nova senha: "), read(NewPassword),
     writeln("Novo cargo (administrador | gerente | vendedor): "), read(NewRole),
     update_user(CPF, NewPassword, NewRole), nl,
-    writeln('********** Usuário atualizado com sucesso! **********'), nl, nl
+    exibir_mensagem_formatada('✓ Usuário atualizado com sucesso!'), nl, nl
   ;
-    writeln("\n************** Usuário não encontrado! **************")
+    exibir_mensagem_formatada('✗ Usuário não encontrado!')
   ),
   menu_user.
 
@@ -53,9 +52,9 @@ handle_user_option(5) :-
   writeln("CPF do usuário para deletar: "), read(CPF),
   ( user_exists_cpf(CPF) ->
     delete_user(CPF),
-    writeln('*********** Usuário excluído com sucesso! ***********'), nl, nl
+    exibir_mensagem_formatada('✓ Usuário excluído com sucesso!'), nl, nl
   ;
-    writeln("\n************** Usuário não encontrado! **************"), nl, nl
+    exibir_mensagem_formatada('✗ Usuário não encontrado!'), nl, nl
   ),
   menu_user.
 
@@ -63,7 +62,7 @@ handle_user_option(0) :-
   writeln("\n<--- Voltando ao Menu Principal").
 
 handle_user_option(_) :-
-  writeln("Opção inválida. Tente novamente."),
+  exibir_mensagem_formatada("✗ Opção inválida. Tente novamente."),
   menu_user.
 
 exibir_mensagem_formatada(Mensagem) :-
@@ -75,4 +74,4 @@ exibir_mensagem_formatada(Mensagem) :-
     EspacoDireita is EspacoTotal - EspacoEsquerda,
     format('~` t~*|~w~` t~*|~n', [EspacoEsquerda, Mensagem, EspacoDireita]),
     format('~`-t~168|~n'),
-    nl. % Pula uma linha após a mensagem
+    nl.
