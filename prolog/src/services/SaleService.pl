79521aedc441803d15ef7500f631f1e3e9f9f57c
rutesilvva <rute.silva@ccc.ufcg.edu.br>
2024-10-06 19:42:06 -0300

    adicionando erros de tratamento em nova compra

--- prolog/src/services/SaleService.pl
+++ prolog/src/services/SaleService.pl
@@ -29,12 +29,51 @@ handle_sale_option(_) :-
 
 create_sale :- 
     writeln('CPF do Cliente: '), read(CPF),
+    ( \+ valid_cpf(CPF) -> 
+        writeln('** Erro: CPF inválido. O CPF deve conter 11 dígitos numéricos. **'), fail
+    ; true),
+    
+   
     writeln('ID do Vendedor: '), read(SellerId),
+    ( \+ integer(SellerId) -> 
+        writeln('** Erro: O ID do vendedor deve ser um número inteiro. **'), fail
+    ; true),
+
+
     writeln('Data da Venda (YYYY-MM-DD): '), read(DateSale),
+    ( \+ valid_date(DateSale) -> 
+        writeln('** Erro: Data da venda inválida. Use o formato YYYY-MM-DD. **'), fail
+    ; true),
+
+ 
     writeln('Valor da Venda (9.99): '), read(TotalSale),
+    ( \+ valid_price(TotalSale) -> 
+        writeln('** Erro: Valor da venda inválido. Insira um valor numérico. **'), fail
+    ; true),
+    
+    
     generate_new_sale_id(SaleId),  % Função para gerar ID único
-    assertz(sale(SaleId, CPF, SellerId, DateSale, TotalSale, [])),  % Adiciona a nova venda
+    assertz(sale(SaleId, CPF, SellerId, DateSale, TotalSale, [])),  
     format("\n** Venda cadastrada com sucesso! ID da Venda: ~w **\n", [SaleId]).
+
+
+valid_cpf(CPF) :-
+    atom_chars(CPF, Digits),
+    length(Digits, 11),  % O CPF deve ter exatamente 11 dígitos
+    maplist(char_type, Digits, digit).  % Verifica se todos os caracteres são dígitos
+
+
+valid_date(DateStr) :- 
+    split_string(DateStr, "-", "", [Year, Month, Day]),
+    string_length(Year, 4), string_length(Month, 2), string_length(Day, 2),
+    catch(number_string(_, Year), _, fail),
+    catch(number_string(_, Month), _, fail),
+    catch(number_string(_, Day), _, fail).
+
+
+valid_price(Price) :- 
+    number(Price), 
+    Price > 0.
 
 
 generate_new_sale_id(NewID) :-
