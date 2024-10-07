aaff23a286c4b9624dd1e778412dc08ed48ff842
hiago aires <hiago.emanuel.aires.diniz@ccc.ufcg.edu.br>
2024-10-07 08:46:56 -0300

    atualizacao do modelo Client

--- prolog/src/models/Client.pl
+++ prolog/src/models/Client.pl
@@ -1,11 +1,54 @@
-:- module(client, [client/6, show_client/1]).
+:- dynamic client/6.
 
-client(Name, Age, CPF, Address, Phone, Sales).
+load_clients :-
+    exists_file('../data/clientDB.pl'),
+    consult('../data/clientDB.pl'), !;
+    open('../data/clientDB.pl', write, Stream), close(Stream).
 
-show_client(client(Name, Age, CPF, Address, Phone, Sales)) :-
-    format('Client Name: ~w~n', [Name]),
-    format('Age: ~d~n', [Age]),
-    format('CPF: ~w~n', [CPF]),
-    format('Address: ~w~n', [Address]),
-    format('Phone: ~w~n', [Phone]),
-    format('Sales: ~w~n', [Sales]).
+save_client :-
+    tell('../data/clientDB.pl'),
+    listing(client),
+    told.
+
+create_client(Name, Age, CPF, Address, Phone, Sales). :-
+    assertz(client(Name, Age, CPF, Address, Phone, Sales)),
+    save_client.
+
+client_exists_name(Name) :-
+    list_all_clients(Clients),
+    member(client(Name, _, _, _, _, _), Clients).
+
+client_exists_cpf(CPF) :-
+    list_all_clients(Clients),
+    member(product(_, _, CPF, _, _,_), Clients).
+
+get_product_by_cpf(CPF, Client) :-
+    list_all_clients(Clients),
+    member(Client, Clients),
+    Client = client(_, _, CPF, _, _, _).
+
+delete_product(CPF) :-
+    list_all_clients(Clients),
+    member(Client, Clients),
+    Client = client(_, _, CPF, _, _, _),
+    retract(Client),
+    save_client.
+
+update_client(Name, Age, CPF, Address, Phone, Sales) :-
+    list_all_clients(Clients),
+    member(Client, Clients),
+    OldClient = client(_, _, CPF, _, _, _),
+    retract(OldClient),
+    assertz(product(_, _, CPF, _, _, _)),
+    save_client.
+
+list_all_clients(Clients) :-
+    findall(client(Name, Age, CPF, Address, Phone, Sales),
+            client(Name, Age, CPF, Address, Phone, Sales),
+            Clients).
+
+print_clients([]).
+print_clients([client(Name, Age, CPF, Address, Phone, Sales) | T]) :-
+    format('Nome: ~w\nIdade: ~w\nEndereço: ~w\nTelefone: ~w\nVendas: ~d\n----------------------------------------\n',
+           [Name, Age, CPF, Address, Phone, Sales]),
+    print_clients(T).
